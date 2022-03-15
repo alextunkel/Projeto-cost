543714ada37d47fb3395ed718dd2d6d9d65d40b6
alextunkel <95252065+alextunkel@users.noreply.github.com>
2022-03-14 22:39:48 -0300

    projeto quase finalizado

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Container from './components/layout/Container';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Projects from './components/pages/Projects';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/company">
+            <Company />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/NewProject">
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
